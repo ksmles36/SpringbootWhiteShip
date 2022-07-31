5b13f9272d3efcee5481332defb3590ac0cd6e0a
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-07-31 10:44:13 +0900

    test1

--- SpringbootWhiteShipApplication.java
+++ SpringbootWhiteShipApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootWhiteShipApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringbootWhiteShipApplication.class, args);
     }
 
