cc199b80720fe5a6b5a391769bee27329de4f283
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-07-31 10:40:27 +0900

    Initial commit

---
+++ SpringbootWhiteShipApplication.java
@@ -0,0 +1,13 @@
+package com.ksm.springbootwhiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWhiteShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWhiteShipApplication.class, args);
+    }
+
+}
